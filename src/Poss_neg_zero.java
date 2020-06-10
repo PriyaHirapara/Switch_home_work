e868c639d186b7eba3958846cbb40ccce2b86fb8
priyankamendpara <priya.soft.test@gmail.com>
2020-06-10 17:03:09 +0530

    'reserved'

--- src/Poss_neg_zero.java
+++ src/Poss_neg_zero.java
@@ -1,19 +1,22 @@
+import java.util.Scanner;
 
 public class Poss_neg_zero { // class name
     public static void main(String args[]) { //main method
+        Scanner scanner = new Scanner(System.in); // creat object for scanner class
+        System.out.println("please enter number "); // printing out  please enter number
+        int number = scanner.nextInt(); //int number is
 
-        int number = 30 ; //int number is
-
-        switch (number) {
-            case  1: // if user enter 1 than message comes  Number is possitive
-                System.out.println("Number is possitive");
+        switch (number >= 0 ? 1 : 0) { //
+            case 0:
+                System.out.println("your number is negative ");
                 break;
-            case 30: // if user enter int number than number is negative
-                System.out.println("Number is negetive ");
-                break;
-            default:
-                System.out.println("Number is zero");
-        }
-    }
-}
+            case 1:
+                switch (number > 0 ? 1 : 0) {
+                    case 0: // if number is 0 than printing out zero
+                        System.out.println("your number is zero ");
+                        break;
+                    case 1: // if number is grether then or equle to 1 than printing out positive
+                        System.out.println(" your number is positive");
+                        break;
+                } } }}
 
--- src/Rivers_String.java
+++ src/Rivers_String.java
@@ -1,25 +1,18 @@
-import java.util.Scanner;
 
 public class Rivers_String {
+
     public static void main(String args[]) { // main method
-        String r = reverse("abcdf"); // make string object
-        System.out.println(r); //printing out  r
-    }
-    public static String reverse(String s) { // creat reverse method
-        char[] letters = new char[s.length()]; // creat cher as object
 
-        int letterindex = 0; //take int as 0
-        for (int i = s.length() - 1; i >= 0; i--) {
-            letters[letterindex] = s.charAt(i);
-            letterindex ++;
-        }
-        String reverse = ""; //convert string reverse
-        for (int i=0; i< s.length(); i ++){
-            reverse = reverse + letters[i];
-        }
-        return reverse; // final order to string
+       StringBuilder a = new StringBuilder("abcdef"); // creat a object for stringbuilder class
+       System.out.println("string is = "+ a); // printing out stringBuilder
+       System.out.println("reverse of string is = "+ a.reverse()); // reversing of stringbuilder
 
+        }
 
     }
-}
+
+
+
+
+
 
